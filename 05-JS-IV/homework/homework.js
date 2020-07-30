1a315455a241b77e678610c54f5a81719cef7e6f
Ariel Gentile <arielgentile89@gmail.com>
2020-07-30 13:14:45 -0300

    nueva subida

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -1,5 +1,7 @@
 // No cambies los nombres de las funciones.
 
+// subido al repo //
+
 function crearGato(nombre, edad) {
   // Crear un nuevo objeto con la propiedad "nombre" y el valor definido como el argumento "nombre".
   // Agrega una propiedad al objeto con el nombre "edad" y usa el valor definido en el argumento "edad"
